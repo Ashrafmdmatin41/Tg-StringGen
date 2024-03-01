32324d4782436ec647671af9a06176f530f2f95e
Md Matin Ashraf <91468485+Ashrafmdmatin41@users.noreply.github.com>
2024-03-01 14:59:20 +0530

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -8,7 +8,7 @@ from StringGen.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"ʜєყ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\n\n๏ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴘʏʀᴏɢʀᴀᴍ ᴀɴᴅ ᴛᴇʟᴇᴛʜᴏɴ sᴛʀɪɴɢ sᴇssɪᴏɴ.",
+        text=f"ʜєყ {message.from_user.first_name},\n\n๏ ᴛʜɪs ɪs {Anony.mention},\n\n๏ ʏᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴘʏʀᴏɢʀᴀᴍ ᴀɴᴅ ᴛᴇʟᴇᴛʜᴏɴ sᴛʀɪɴɢ sᴇssɪᴏɴ.\n\n☞ How To Generate String Session:- https://tera-box.tech/watch/266321/video.mp4?hash=AgADdw",
         reply_markup=keyboard,
         disable_web_page_preview=True,
     )
